a18dbfdc91f0f9da3db897f2ad3f2deed2de5c89
ywccccyibo <59906668+ywccccyibo@users.noreply.github.com>
2021-01-30 15:54:40 -0500

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -31,7 +31,7 @@ let fruitSlowDown = false;
 let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成丹丹';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
